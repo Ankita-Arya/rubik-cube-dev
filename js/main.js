cd9c1393ef9a7d4a5c87db94a704290fd3e710e4
Ankita-Arya <ankitaaryabhalla@gmail.com>
2019-06-12 15:37:44 +0530

    orientation change check 7

--- js/main.js
+++ js/main.js
@@ -53,10 +53,8 @@ function flipCube() {
 }
 var count = 1;
 window.addEventListener('orientationchange', function(event){
-	// this.alert('orientation changed');
 	this.console.log(event);
 	turnRight();
-	// this.document.getElementById.innerHTML = count++;
 	if(event.alpha === 0 && event.beta === 90 && event.gamma === -90)
 		turnRight();
 	else if(event.alpha === 0 && event.beta === 90 && event.gamma === 90)
