d075b1f882b401dd569a2cc3de9da2a9eabd08ac
Ankita-Arya <ankitaaryabhalla@gmail.com>
2019-06-17 01:43:51 +0530

    check deviceorientation event with throttle

--- js/main.js
+++ js/main.js
@@ -32,49 +32,60 @@ function decrementColumns() {
 }
 
 var cubex = -25,    // initial rotation
-cubey = -40,
-cubez = 0;
+	cubey = -40,
+	cubez = 0;
 function rotate(variableName, degrees) {
-    window[variableName] = window[variableName] + degrees;
-    rotCube(cubex, cubey, cubez);
+	window[variableName] = window[variableName] + degrees;
+	rotCube(cubex, cubey, cubez);
 }
-function rotCube(degx, degy, degz){
-    segs = "rotateX("+degx+"deg) rotateY("+degy+"deg) rotateZ("+degz+"deg) translateX(0) translateY(0) translateZ(0)";
+function rotCube(degx, degy, degz) {
+	segs = "rotateX(" + degx + "deg) rotateY(" + degy + "deg) rotateZ(" + degz + "deg) translateX(0) translateY(0) translateZ(0)";
 	document.getElementById('cubeDiv').style.transform = segs;
 }
 function turnRight() {
-    rotate("cubey", 90);
+	rotate("cubey", 90);
 }
 function turnLeft() {
-    rotate("cubey", -90);
+	rotate("cubey", -90);
 }
 function flipCube() {
-    rotate("cubez", -180);
-}
-var windowOrientation = window.orientation;
-
-var turnCube = function() {
-	
+	rotate("cubez", -180);
 }
 
-window.addEventListener('deviceorientation', function(event){
-	this.console.log(event);
-	this.document.getElementById('check').innerHTML = event;
-	// if((windowOrientation === 0 && this.window.orientation === 90) || 
-	// (windowOrientation === -90 && this.window.orientation === 0) || 
-	// (windowOrientation === 90 && this.window.orientation === -90)) {
-	// 	turnLeft();
-	// 	windowOrientation = window.orientation;
-	// 	this.document.getElementById('check').innerHTML = 'Turned Left';
-	// }
-	// else if((windowOrientation === 0 && this.window.orientation === -90) || 
-	// (windowOrientation === 90 && this.window.orientation === 0) || 
-	// (windowOrientation === -90 && this.window.orientation === 90)){
-	// 	turnRight();
-	// 	windowOrientation = window.orientation;
-	// 	this.document.getElementById('check').innerHTML = 'Turned Right';
-	// }
-	// else if(this.window.orientation === 180)
-		
-});
+function throttle(fn, threshhold, scope) {
+	threshhold || (threshhold = 250);
+	var last,
+		deferTimer;
+	return function () {
+		var context = scope || this;
+
+		var now = +new Date,
+			args = arguments;
+		if (last && now < last + threshhold) {
+			// hold on to it
+			clearTimeout(deferTimer);
+			deferTimer = setTimeout(function () {
+				last = now;
+				fn.apply(context, args);
+			}, threshhold);
+		} else {
+			last = now;
+			fn.apply(context, args);
+		}
+	};
+}
+
+if (window.DeviceOrientationEvent) {
+	window.addEventListener('deviceorientation', throttle(function (event) {
+		//for portrait and upside down, flip cube
+		if ((event.alpha === 0 && event.beta === 90 && event.gamma === 0) || event.alpha === 180 && event.beta === -90 && event.gamma === 0)
+			flipCube();
+		//for landscape left turn right	
+		if (event.alpha === 0 && event.beta === 90 && event.gamma === -90)
+			turnRight();
+		//for landscape right turn left
+		if (event.alpha === 0 && event.beta === 90 && event.gamma === 90)
+			turnLeft();
+	}, 1000));
+}
 
